e9799f7b80c1018b29120fb785d742a5f2f85116
yves mugisha <yvesmugisha84@gmail.com>
2024-05-13 07:04:50 +0200

    fix:on the editor

---
+++ client/src/App.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import TextEditor from "./TextEditor";
+import {
+  BrowserRouter as Router,
+  Route,
+  Navigate,
+  Routes,
+} from "react-router-dom";
+import { v4 as uuidV4 } from "uuid";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to={`/documents/${uuidV4()}`} />} />
+        <Route path="/documents/:id" element={<TextEditor />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
